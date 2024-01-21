83f02a3b4a81f8c4e6f84ff04c9d8727e2e62203
ku18m <133920951+ku18m@users.noreply.github.com>
2024-01-22 00:08:27 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
+#!/usr/bin/pup
 # Install flask from pip3 v2.1.0
-package { 'flask' :
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }

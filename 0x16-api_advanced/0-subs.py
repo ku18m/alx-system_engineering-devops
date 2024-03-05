15506d23de443f7f967708263948ba767aacb820
Mohamed Kamal <ku18mz@gmail.com>
2024-03-06 00:56:36 +0200

    Fix handling of invalid subreddit input and error response in number_of_subscribers function

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Get number of subscribers for a given subreddit"""
-    if not subreddit or type(subreddit) is not str:
+    if subreddit is None or type(subreddit) is not str:
         return 0
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
@@ -15,4 +15,6 @@ def number_of_subscribers(subreddit):
     }
     response = requests.get(
         url, headers=headers, allow_redirects=False, timeout=10)
-    return response.json().get('data', {}).get('subscribers', 0)
+    if response.status_code != 200:
+        return 0
+    return response.json().get('data').get('subscribers')

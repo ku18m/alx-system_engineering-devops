ca24da3629dc4834cfe227f39b980bc31c107588
Mohamed Kamal <ku18mz@gmail.com>
2024-03-06 12:47:22 +0200

    make it more simple by using sed instead of reading the whole content file then writing it

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,8 @@
 # File: site.pp
 
-# Define the path to the WordPress settings file
-$wordpress_settings_file = '/var/www/html/wp-settings.php'
+# Replaces occurrences of 'phpp' extensions with 'php' in the WordPress file `wp-settings.php`.
 
-# Manage the WordPress settings file
-file { $wordpress_settings_file:
-  ensure  => file,
-  content => inline_template("
-<?php
-# Managed by Puppet
-
-# Read the current content of the file
-${current_content} = file_get_contents('${wordpress_settings_file}');
-
-# Replace 'phpp' extensions with 'php'
-${new_content} = str_replace('phpp', 'php', ${current_content});
-
-echo ${new_content};
-"),
-  notify  => Service['apache'],
+exec { 'fix-wordpress-file':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }

a581e87ddf09c63f0ba19edd721ef15b26bf6da4
Mohamed Kamal <ku18mz@gmail.com>
2024-03-06 12:29:00 +0200

    take another try for unknown server error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,45 +1,25 @@
 # File: site.pp
 
-# Define a class for Apache installation and configuration
-class { 'apache':
-  service_ensure => 'running',  # Ensure Apache service is running
-}
-
-# Define a class for PHP installation and configuration
-class { 'php':
-  package_ensure => 'present',  # Ensure PHP package is installed
-}
-
-# Check and set permissions for the directories used by Apache
-file { '/var/www/html':
-  ensure => 'directory',
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-# Check Apache configuration files for errors
-exec { 'check_apache_config':
-  command     => 'apache2ctl configtest',
-  refreshonly => true,
-  notify      => Service['apache'],
-}
-
-# Check PHP configuration files for errors
-exec { 'check_php_config':
-  command     => 'php -v',
-  refreshonly => true,
+# Check server resources
+exec { 'check_server_resources':
+  command => 'free -m && df -h',
 }
 
 # Update server software
 package { 'update_server_packages':
-  ensure  => 'latest',
-  require => Exec['check_apache_config', 'check_php_config'],
+  ensure => 'latest',
 }
 
-# Restart Apache service if configuration changes or updates are applied
+# Restart Apache service
 service { 'apache':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => [File['/var/www/html'], Exec['check_apache_config'], Package['update_server_packages']],
+  ensure  => 'running',
+  enable  => true,
+  require => Package['update_server_packages'],
+}
+
+# Restart PHP service
+service { 'php':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['update_server_packages'],
 }

14b19d8b444cc641bc16ebc4573ad2043b9fb82f
Mohamed Kamal <ku18mz@gmail.com>
2024-03-06 12:32:35 +0200

    give another try

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,25 +1,32 @@
 # File: site.pp
 
-# Check server resources
-exec { 'check_server_resources':
-  command => 'free -m && df -h',
+# Ensure Apache is installed and running
+class { 'apache':
+  service_ensure => 'running',
 }
 
-# Update server software
-package { 'update_server_packages':
-  ensure => 'latest',
+# Ensure PHP is installed
+class { 'php':
+  package_ensure => 'present',
 }
 
-# Restart Apache service
+# Set up Apache configuration
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => file,
+  content => template('apache/000-default.conf.erb'),
+  notify  => Service['apache'],
+}
+
+# Set up PHP configuration
+file { '/etc/php/5.5/apache2/php.ini':
+  ensure  => file,
+  content => template('php/php.ini.erb'),
+  notify  => Service['apache'],
+}
+
+# Restart Apache service after configuration changes
 service { 'apache':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['update_server_packages'],
-}
-
-# Restart PHP service
-service { 'php':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['update_server_packages'],
+  ensure    => 'running',
+  enable    => true,
+  subscribe => [File['/etc/apache2/sites-available/000-default.conf'], File['/etc/php/5.5/apache2/php.ini']],
 }

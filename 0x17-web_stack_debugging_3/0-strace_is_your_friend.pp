5dbe1022e24e534b41961374e62e985035bae388
Mohamed Kamal <ku18mz@gmail.com>
2024-03-06 12:44:56 +0200

    remove unnecessary instructions

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,5 @@
 # File: site.pp
 
-# Ensure Apache is installed and running
-class { 'apache':
-  service_ensure => 'running',
-}
-
-# Ensure PHP is installed
-class { 'php':
-  package_ensure => 'present',
-}
-
 # Define the path to the WordPress settings file
 $wordpress_settings_file = '/var/www/html/wp-settings.php'
 
